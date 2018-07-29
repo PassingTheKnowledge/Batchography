ab63e95ba770304b010535cda8243339018b869a
Elias Bachaalany <elias@passingtheknowledge.net>
2018-07-29 14:39:40 -0700

    WifiPasswordReveal: Added French language support

--- WifiPasswordReveal.bat
+++ WifiPasswordReveal.bat
@@ -8,12 +8,37 @@
 setlocal enabledelayedexpansion
 
 :main
-    title WiFiPasswordReveal v1.0 (c) lallouslab.net - The Batchography book
+    title WiFiPasswordReveal v1.1 (c) lallouslab.net - The Batchography book
     
     echo.
-    echo Reveal all saved WiFi passwords Batch file script v1.0 (c) lallouslab.net
+    echo Reveal all saved WiFi passwords Batch file script v1.1 (c) lallouslab.net
     echo.
     echo Inspired by the book "Batchography: The Art of Batch Files Programming"
+    echo.
+
+
+    :: Default language is English
+    set KEYCONTENT=Key Content
+    set ALL_PROFILES=All User Profile
+
+    :: Detect the language
+    for /F "usebackq tokens=3" %%a IN (`reg query "hklm\system\controlset001\control\nls\language" /v Installlanguage`) DO (
+        :: 0409 English ; 0407 German ; 040C French ; 0C0A Spanish
+        :: https://docs.microsoft.com/en-us/previous-versions/office/developer/speech-technologies/hh361638(v=office.14)
+        set lang_id=%%a
+    )
+
+    if "%lang_id%"=="0409" (
+        echo English operating system language detected
+    ) else if "%lang_id%" == "040C" (
+        set KEYCONTENT=Contenu de la cl
+        set ALL_PROFILES=Profil Tous les utilisateurs
+
+        echo French operating system language detected
+    ) else (
+        echo Warning: Unknown operating system language detected! The script might not work correctly!
+    )
+
     echo.
 
     :: Get all the profiles
